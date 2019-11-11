76b0f4143c88da528da7921d234c8cd220bb29f1
peckyboy <31924107+peckyboy@users.noreply.github.com>
2019-11-11 23:19:37 +0100

    Update sw.js
    Service worker

--- sw.js
+++ sw.js
@@ -3,15 +3,7 @@ const assets = ['/','index.html','./assets/main.js',"https://cdnjs.cloudflare.co
 self.addEventListener('install',e=>{
     e.waitUntil(caches.open(cachename).then(cache=>cache.addAll(assets)).then(err=>console.log(err)))
 })
-self.addEventListener('activate',e=>{
-    e.waitUntil(
-        caches.keys().then(keys=>{
-            return Promise.all(
-                keys.filter(key=>key!==cachename).map(key=>caches.delete(key))
-            )
-        })
-    )
-})
+
 self.addEventListener('fetch',e=>{
     e.respondWith(async function() {
         const cachedResponse = await caches.match(e.request);
