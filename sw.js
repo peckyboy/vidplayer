7c3a76908bb50ad9ffca0c2debcd9d821dae797d
peckyboy <31924107+peckyboy@users.noreply.github.com>
2019-11-12 08:43:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cachename = 'vid-1.11'
+const cachename = 'vid-2.0'
 const assets = ['vidplayer/','vidplayer/index.html','vidplayer/assets/main.js',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css","https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"]
 self.addEventListener('install',e=>{
     e.waitUntil(caches.open(cachename).then(cache=>cache.addAll(assets)).then(err=>console.log(err)))
