1a1dd740eea89d9564eecd8483682877469093ed
peckyboy <31924107+peckyboy@users.noreply.github.com>
2019-11-20 05:53:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cachename = 'vid-3.1'
-const assets = ['/vidplayer/','/vidplayer/index.html','/vidplayer/assets/main.js',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css","https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"]
+const assets = ['/vidplayer/','/vidplayer/index.html','/vidplayer/assets/main.js',"/vidplayer/assets/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2","/vidplayer/assets/materialize.min.css"]
 self.addEventListener('install',e=>{
     e.waitUntil(
         caches.open(cachename)
